5fd71cc23900a565df19a320d650ea2db990e744
Zain <syedzainnaqvi3324@gmail.com>
2022-02-27 15:11:31 +0500

    first change just check working or not

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Material(
+        child: Center(
+      child: Container(
+        child: Text('Hi flutter '),
+      ),
+    )),
+  ));
+}
